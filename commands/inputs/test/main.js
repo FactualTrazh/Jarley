11f0efa95a66168f5bda5871e7e4802f4bc7234b
MarzonDark <marzon.dark@gmail.com>
2021-11-01 17:20:16 -0400

    Comando de prueba actualizado

--- commands/inputs/test/main.js
+++ commands/inputs/test/main.js
@@ -8,17 +8,12 @@ module.exports = {
     
     flags: [ 'dm', 'guild' ],
 
-    intents: [
-
-        discord.Intents.FLAGS.GUILDS
-    ],
-
     events: {
     
         command: function ({ client, event, loaders, databases, bases, utils }) {
 
             const messageEmbed = new discord.MessageEmbed()
-            .setDescription(`La **ID** de este canal es **${event.channel?.id ?? 'Indefinida por ejecutarse desde un DM o por falta de permisos'}**`)
+            .setDescription(`Hola ${event.user.toString()}`)
             .setColor('#FFFFFF');
     
             event.editReply({ embeds: [ messageEmbed ] }).catch(() => {});
